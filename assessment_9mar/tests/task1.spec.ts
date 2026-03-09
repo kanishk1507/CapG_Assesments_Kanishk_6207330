b6994b3be09c264c925109df559ec216b62372a5
Kanishk Acharya <115039381+kanishk1507@users.noreply.github.com>
2026-03-09 16:30:13 +0530

    task1 done

---
+++ assessment_9mar/tests/task1.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Login scenario - QSpiders Demo', async ({ page }) => {
+
+  test.setTimeout(20000);
+  await page.goto('https://demoapps.qspiders.com/ui/login');
+
+  await page.getByLabel('Email Id').fill('admin@gmail.com');
+  await page.getByLabel('Password').fill('admin@123');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+//   await expect(page.getByText('Dashboard')).toBeVisible();
+//   await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+
+await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+await expect(page).toHaveScreenshot("task1.png");
+});
