4e081e02314322869ce7a2a7923dc818dd1707f1
Kanishk Acharya <115039381+kanishk1507@users.noreply.github.com>
2026-03-18 19:34:18 +0530

    18th march done

---
+++ assessment_18mar/tests/task1.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+import { UploadPage } from "../PageObjectModel/task1.page";
+import fs from "fs";
+import path from "path";
+
+const data = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../testdata/task1.json"), "utf-8")
+);
+test("E2E File Upload → Validate Uploaded File Name", async ({ page }) => {
+  const uploadPage = new UploadPage(page);
+  await uploadPage.navigate();
+  await uploadPage.uploadFile(data.filePath);
+  await uploadPage.clickUpload();
+  const actualFileName = await uploadPage.getUploadedFileName();
+  expect(actualFileName?.trim()).toBe(data.expectedFileName);
+
+  await page.screenshot({
+    path: "screenshot/task1-successful.png",
+    fullPage: true,
+  });
+});
